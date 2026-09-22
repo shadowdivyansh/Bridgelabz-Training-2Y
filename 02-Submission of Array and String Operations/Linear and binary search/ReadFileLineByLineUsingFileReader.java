adecbf0f72feb8aa65a027d29d4f3abcd9306bf8
Divyansh Singh <divyanshsinghfr@gmail.com>
2026-09-22 11:55:38 +0530

    Delete 02-Submission of Array and String Operations directory

--- 02-Submission of Array and String Operations/Linear and binary search/ReadFileLineByLineUsingFileReader.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-public class ReadFileLineByLineUsingFileReader{
-public static void main(String[] args)throws Exception{
-BufferedReader br=new BufferedReader(new FileReader("input.txt"));
-String line;
-while((line=br.readLine())!=null) System.out.println(line);
-br.close();
-}
-}
--- 02-Submission of Array and String Operations/String/Level-1/IllegalArgumentException Demo.java
+++
@@ -1,9 +0,0 @@
-class IllegalArgumentExceptionDemo{
-    public static void main(String[] args){
-        try{
-            throw new IllegalArgumentException("Invalid Argument");
-        }catch(IllegalArgumentException e){
-            System.out.println("IllegalArgumentException");
-        }
-    }
-}
