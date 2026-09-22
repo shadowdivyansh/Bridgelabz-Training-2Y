d74c25839d635d76a835e8dc0d625936031046aa
Divyansh Singh <divyanshsinghfr@gmail.com>
2026-09-22 11:50:15 +0530

    Delete 01-Practice-Problems directory

--- 01-Practice-Problems/Assisted Problem/FahrenheitConversion.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class FahrenheitConversion{
-	public static void main(String args[]){
-	Scanner sc=new Scanner(System.in);
-	double c=sc.nextInt();
-	double f=(c * 9/5)+32;
-	System.out.println(f);
-	}
-	}
